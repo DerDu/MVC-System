ce4c6771ad4f3dfcfe73a8386e64dff0054ff0a5
DerDu <blood-demon@gmx.net>
2011-05-31 10:57:04 +0200

    [Code cleanup]

--- MVCModel.php
+++ MVCModel.php
@@ -1,7 +1,10 @@
 <?php
 namespace MVCSystem;
 use \AIOSystem\Api\Database;
+use \AIOSystem\Api\Event;
+
 abstract class MVCModel {
+	const DEBUG = false;
 	/**
 	 * @var null|string $ViewMethod
 	 */
@@ -11,6 +14,7 @@ abstract class MVCModel {
 	 * @return null|string
 	 */
 	protected function ModelViewMethod( $ModelViewMethod = null ) {
+		if(self::DEBUG){Event::Message(__METHOD__,__FILE__,__LINE__);}
 		if( $ModelViewMethod !== null ) {
 			$this->ModelViewMethod = $ModelViewMethod;
 		} return $this->ModelViewMethod;
@@ -24,6 +28,7 @@ abstract class MVCModel {
 	 * @return null|int
 	 */
 	protected function ModelId( $ModelId = null ) {
+		if(self::DEBUG){Event::Message(__METHOD__,__FILE__,__LINE__);}
 		if( $ModelId !== null ) {
 			$this->ModelId = $ModelId;
 		} return $this->ModelId;
@@ -36,18 +41,22 @@ abstract class MVCModel {
 	 * @return bool
 	 */
 	protected function DatabaseEdit( $Table, $Fieldset = array(), $Where = null, $Delete = false ) {
+		if(self::DEBUG){Event::Message(__METHOD__,__FILE__,__LINE__);}
 		return Database::Record( $Table, $Fieldset, $Where, $Delete );
 	}
 	/**
 	 * @param string $Table
 	 * @param string $Where
 	 * @param null|string $OrderBy
+	 * @param bool $ResultSetAsArray
 	 * @return array
 	 */
 	protected function DatabaseGet( $Table, $Where, $OrderBy = null, $ResultSetAsArray = false ) {
+		if(self::DEBUG){Event::Message(__METHOD__,__FILE__,__LINE__);}
 		return Database::RecordSet( $Table, $Where.(($OrderBy === null)?'':' ORDER BY '.$OrderBy), $ResultSetAsArray );
 	}
 	protected function DatabaseUtf8( $DataArray ) {
+		if(self::DEBUG){Event::Message(__METHOD__,__FILE__,__LINE__);}
 		foreach( (array)$DataArray as $Index => $Content ) {
 			$DataArray[$Index] = array_map( '\AIOSystem\Api\Font::MixedToUtf8', $Content );
 		}
