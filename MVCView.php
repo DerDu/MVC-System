4cc53e08a30e7f250e1870ece7d1c8de78d857a2
DerDu <blood-demon@gmx.net>
2011-06-16 22:59:08 +0200

    [Update] Cache possibilities [Update] Template fetching

--- MVCView.php
+++ MVCView.php
@@ -1,10 +1,12 @@
 <?php
 namespace MVCSystem;
+use \AIOSystem\Api\System;
 use \AIOSystem\Api\Template;
 use \AIOSystem\Api\Stack;
 use \AIOSystem\Api\Cache;
 use \AIOSystem\Api\Event;
 abstract class MVCView {
+	const DEBUG = false;
 	/** @var \AIOSystem\Core\ClassStackRegister|null */
 	private $DataRegister = null;
 
@@ -38,28 +40,43 @@ abstract class MVCView {
 	 * @param int $Seconds
 	 * @return string
 	 */
-	public function setCache( $Seconds = 3600 ) {
-		Cache::Set( $this->idCache(), ($Display = $this->Display()), get_class( $this ), false, $Seconds );
+	public function setCache( $Seconds = 3600, $InstanceId = null ) {
+		Cache::Set( ($InstanceId===null?$this->idCache():$InstanceId), ($Display = $this->Display()), get_class( $this ), false, $Seconds );
 		return $Display;
 	}
 	/**
 	 * Return display cache or false
 	 *
+	 * @param bool $useInstanceId
 	 * @return false|string
 	 */
-	public function getCache() {
-		return Cache::Get( $this->idCache(), get_class($this), false );
+	public function getCache( $useInstanceId = false ) {
+		return Cache::Get( $this->idCache( $useInstanceId ), get_class($this), false );
+	}
+	/**
+	 * Return cache time
+	 *
+	 * @param bool $useInstanceId
+	 * @return int
+	 */
+	public function getCacheTime( $useInstanceId = false ) {
+		return Cache::GetTime( $this->idCache( $useInstanceId ), get_class($this), false );
 	}
 	/**
 	 * Create cache identifier
 	 *
+	 * @param bool $useInstanceId
 	 * @return string
 	 */
-	private function idCache() {
-		return sha1( get_class( $this )
-			.serialize( get_class_methods( get_class( $this ) ) )
-			.serialize( get_class_vars( get_class( $this ) ) )
-		);
+	public function idCache( $useInstanceId = false ) {
+		if( $useInstanceId ) {
+			return sha1( serialize( $this ) );
+		} else {
+			return sha1( get_class( $this )
+				.serialize( get_class_methods( get_class( $this ) ) )
+				.serialize( get_class_vars( get_class( $this ) ) )
+			);
+		}
 	}
 
 	/**
@@ -73,22 +90,28 @@ abstract class MVCView {
 	public function Template( $Name, $ParsePhp = true, $ParsePhpAfterContent = false ) {
 		$Folder = 'Template';
 		// Try specific folder at Application
-		if( file_exists( ($Location = preg_replace('!^\\\\!is','', MVCManager::DirectoryApplication().DIRECTORY_SEPARATOR.$this->BaseDirectory().DIRECTORY_SEPARATOR.$Name, 1) ) ) ) {
-			$TemplateFileLocation = $Location;
+		if( file_exists( ($LocationA = System::DirectorySyntax( preg_replace('!^\\\\!is','', MVCManager::DirectoryApplication().DIRECTORY_SEPARATOR.$this->BaseDirectory().DIRECTORY_SEPARATOR.$Name, 1), false, DIRECTORY_SEPARATOR ) ) ) ) {
+			$TemplateFileLocation = $LocationA;
 		// Try generic folder at Application (Template)
-		} else if( file_exists( ($Location = preg_replace('!^\\\\!is','', MVCManager::DirectoryApplication().DIRECTORY_SEPARATOR.dirname($this->BaseDirectory()).DIRECTORY_SEPARATOR.$Folder.DIRECTORY_SEPARATOR.$Name, 1) ) ) ) {
-			$TemplateFileLocation = $Location;
+		} else if( file_exists( ($LocationB = System::DirectorySyntax( preg_replace('!^\\\\!is','', MVCManager::DirectoryApplication().DIRECTORY_SEPARATOR.dirname($this->BaseDirectory()).DIRECTORY_SEPARATOR.$Folder.DIRECTORY_SEPARATOR.$Name, 1), false, DIRECTORY_SEPARATOR ) ) ) ) {
+			$TemplateFileLocation = $LocationB;
 		// Try specific folder at MVC
-		} else if( file_exists( ($Location = __DIR__.DIRECTORY_SEPARATOR.$this->BaseDirectory().DIRECTORY_SEPARATOR.$Name) ) ) {
-			$TemplateFileLocation = $Location;
+		} else if( file_exists( ($LocationC = System::DirectorySyntax( __DIR__.DIRECTORY_SEPARATOR.$this->BaseDirectory().DIRECTORY_SEPARATOR.$Name, false, DIRECTORY_SEPARATOR ) ) ) ) {
+			$TemplateFileLocation = $LocationC;
 		// Try generic folder at MVC (Template)
-		} else if( file_exists( ($Location = __DIR__.DIRECTORY_SEPARATOR.dirname($this->BaseDirectory()).DIRECTORY_SEPARATOR.$Folder.DIRECTORY_SEPARATOR.$Name) ) ) {
-			$TemplateFileLocation = $Location;
+		} else if( file_exists( ($LocationD = System::DirectorySyntax( __DIR__.DIRECTORY_SEPARATOR.dirname($this->BaseDirectory()).DIRECTORY_SEPARATOR.$Folder.DIRECTORY_SEPARATOR.$Name, false, DIRECTORY_SEPARATOR ) ) ) ) {
+			$TemplateFileLocation = $LocationD;
 		// Set location for error information
 		} else {
 			$TemplateFileLocation = $Name;
 		}
-		//Event::Message('TPL Load: '.$TemplateFileLocation);
+		if(self::DEBUG){
+			if(isset($LocationA))Event::Message('TPL Location A: '.$LocationA);
+			if(isset($LocationB))Event::Message('TPL Location B: '.$LocationB);
+			if(isset($LocationC))Event::Message('TPL Location C: '.$LocationC);
+			if(isset($LocationD))Event::Message('TPL Location D: '.$LocationD);
+			Event::Message('Load['.__METHOD__.'] '.$TemplateFileLocation.' ... ');
+		}
 		return Template::Load( $TemplateFileLocation, $ParsePhp, $ParsePhpAfterContent );
 	}
 	/**
